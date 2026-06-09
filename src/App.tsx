f3224f9aab1a9b55742ed69faf38c1a8e9eb6d0c
salioudunn11 <salioudunn11@gmail.comclean>
2026-06-09 13:17:19 -0400

    added count and length

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,69 @@
-//import
-import { useState } from "react";
-
-const Message = ({text}) => {
-// All components have to start with a capital
-    return <>
-    <p>
-        {text}
-    </p>
-   
-    </>;
-    
-}
+import {useState} from 'react';
+import Message from './components/Message';
+import './App.css';
 
 const App = () => {
     //
     // business logic
     //
 
-
-    //  <p> Hello World</p>
-    //  <p>This is a test message</p>
-
-
-
     /*
-    const message = [
+    const messages = [
         "Argentina",
-        "Brasil",
-        "Cambodia"
+        "Bolivia",
+        "Cambodia",
     ];
     */
+   
 
-    const [messages, setMessages] = useState([])
+    const [messages, setMessages ] = useState([]);
+    // Found out using AI that useState only allows two things at once.
+    
+    const [incomingText, setIncomingText ] = useState('');
+
 
     return <>
-    <h1>
-        Chat Room
-    </h1>
-   {
-    message.map((text) => <Message text={text}/>  )
-   }
-   
-    <input />
-    <button>
-        Send
-    </button>
+        <h1>
+            Chat Room
+        </h1>
+        <p>
+            Messages: {messages.length}
+        </p>
 
+        {
+            messages.map(
+                (text, index) => <Message key={index} text={text} />
+                )
+        }
+
+        <form onSubmit={(event) => {
+            event.preventDefault();
+
+            // determine what the new message is
+            const new_message = incomingText;
+
+            // add the new message to the message state
+            setMessages([...messages, new_message]);
+
+            // note: this is not best practice in react but it works for now
+            //@ts-expect-error
+            // document.getElementById('incoming_text').value = '';
+            setIncomingText ('');
+        }}>
+        <input name='incoming_text' 
+               id='incoming_text' 
+               value={incomingText}
+               onChange={(event) => setIncomingText(event.target.value)
+
+               }
+        />
+        <p> 
+            characters:{incomingText.length}
+        </p>
+        <button type='submit'>
+            send
+        </button>
+        </form>
     </>;
 }
 
---
+++ src/components/message.tsx
@@ -0,0 +1,14 @@
+import { faker } from "@faker-js/faker";
+
+const Message = ({ text }) => {
+
+    const name = faker.person.fullName();
+
+    return (
+        <p className="message">
+            {text} - {name}
+        </p>
+    );
+};
+
+export default Message;
